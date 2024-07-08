a583517e0afd94b9a0fca333d334cac28132ee3c
Tarek <114747789+Tarek124@users.noreply.github.com>
2024-07-08 22:20:03 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,37 @@ import gsap from "gsap";
 const App = () => {
   const comp = useRef(null);
 
-  // useLayoutEffect(() => {
-  //   let ctx = gsap.context(() => {
-  //     const t1 = gsap.timeline();
-  //     t1.from("#intro-slider", {
-  //       xPercent: "-100",
-  //       duration: 1.3,
-  //       delay: 0.3,
-  //     })
-  //       .from(["#title-1", "#title-2", "#title-3", "#title-4"], {
-  //         opacity: 0,
-  //         y: "+=30",
-  //         stagger: 0.5,
-  //       })
-  //       .to(["#title-1", "#title-2", "#title-3", "#title-4"], {
-  //         opacity: 0,
-  //         y: "-=30",
-  //         delay: 0.3,
-  //         stagger: 0.5,
-  //       })
-  //       .to("#intro-slider", {
-  //         xPercent: "-100",
-  //         duration: 1.3,
-  //       })
-  //       .from("#welcome", {
-  //         opacity: 0,
-  //         duration: 0.5,
-  //       });
-  //   }, comp);
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      const t1 = gsap.timeline();
+      t1.from("#intro-slider", {
+        xPercent: "-100",
+        duration: 1.3,
+        delay: 0.3,
+      })
+        .from(["#title-1", "#title-2", "#title-3", "#title-4"], {
+          opacity: 0,
+          y: "+=30",
+          stagger: 0.5,
+        })
+        .to(["#title-1", "#title-2", "#title-3", "#title-4"], {
+          opacity: 0,
+          y: "-=30",
+          delay: 0.3,
+          stagger: 0.5,
+        })
+        .to("#intro-slider", {
+          xPercent: "-100",
+          duration: 1.3,
+        })
+        .from("#welcome", {
+          opacity: 0,
+          duration: 0.5,
+        });
+    }, comp);
 
-  //   return () => ctx.revert();
-  // }, []);
+    return () => ctx.revert();
+  }, []);
   return (
     <div className="relative" ref={comp}>
       <div
